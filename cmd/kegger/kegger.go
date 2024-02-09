3d291ed58369c30cab97e405b682173bf61f4cb4
David Beale <david.beale@bealearts.co.uk>
2024-02-09 19:06:00 +0000

    Have to use Fyne in main package

--- cmd/kegger/kegger.go
+++ cmd/kegger/kegger.go
@@ -4,47 +4,73 @@ import (
 	"fmt"
 	"time"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
 	"github.com/bealearts/kegger/internal/brew"
 	. "github.com/bealearts/kegger/internal/logger"
-	"github.com/bealearts/kegger/internal/tray"
-	"github.com/getlantern/systray"
+	"github.com/sqweek/dialog"
 )
 
-var icon, icon2x = tray.CreateTrayIcons()
+// var icon, icon2x = tray.CreateTrayIcons()
+var menu *fyne.Menu
+var updatesMenu *fyne.MenuItem
 
 func main() {
 	defer Logger.Sync()
 
+	ap := app.New()
+
+	if desk, ok := ap.(desktop.App); ok {
+		if icon, err := fyne.LoadResourceFromPath("./assets/kegTemplate@2x.png"); err == nil {
+			desk.SetSystemTrayIcon(icon)
+		}
+
+		menu, updatesMenu = createTrayMenu()
+		desk.SetSystemTrayMenu((menu))
+	}
+
 	Logger.Info("Starting")
-
-	run()
-}
-
-func run() {
-	systray.Run(onReady, onExit)
-	systray.Run(onReady, onExit)
-}
-
-func onReady() {
-	systray.SetTemplateIcon(icon2x, icon)
-	systray.SetTooltip("Kegger - Join the party")
-
-	var updatesMenu, updateAllMenu = tray.CreateTrayMenu()
 
 	ticker := time.NewTicker(time.Hour)
 	go func() {
 		<-ticker.C
-		updateTray(updatesMenu, updateAllMenu)
+		updateTray(menu, updatesMenu)
 	}()
 
-	updateTray(updatesMenu, updateAllMenu)
+	go updateTray(menu, updatesMenu)
+
+	ap.Run()
 }
 
-func onExit() {
-	Logger.Info("Stopping")
+func createTrayMenu() (*fyne.Menu, *fyne.MenuItem) {
+	var menu *fyne.Menu
+	updatesMenu := fyne.NewMenuItem("0 Updates", func() {})
+	updatesMenu.Disabled = true
+	//updatesMenu.ChildMenu = fyne.NewMenu("", fyne.NewMenuItem("Test", func() {}))
+
+	menuItems := []*fyne.MenuItem{
+		updatesMenu,
+
+		fyne.NewMenuItem("Clean up Celler", func() {
+			Logger.Info("Clean up Celler")
+			// updatesMenu.ChildMenu.Items = []*fyne.MenuItem{fyne.NewMenuItem("Changed", func() {
+
+			// })}
+		}),
+		fyne.NewMenuItemSeparator(),
+		fyne.NewMenuItem("About", func() {
+			Logger.Info("About")
+			dialog.Message("%s", "Kegger").Title("About").Info()
+		}),
+	}
+
+	menu = fyne.NewMenu("Kegger", menuItems...)
+
+	return menu, updatesMenu
 }
 
-func updateTray(updatesMenu systray.MenuItem, updateAllMenu systray.MenuItem) {
+func updateTray(*fyne.Menu, *fyne.MenuItem) {
 	Logger.Info("Performing update check")
 	updates, err := brew.CheckForUpdates()
 	if err != nil {
@@ -54,16 +80,26 @@ func updateTray(updatesMenu systray.MenuItem, updateAllMenu systray.MenuItem) {
 
 	fmt.Printf("%+v\n", updates)
 	count := brew.UpdatableCount(updates)
-
-	if count == 0 {
-		systray.SetTemplateIcon(icon2x, icon)
-		updateAllMenu.Disable()
-		updatesMenu.Disable()
+	if count == 1 {
+		updatesMenu.Label = "1 Update Available"
 	} else {
-		systray.SetIcon(icon2x)
-		updateAllMenu.Enable()
-		updatesMenu.Enable()
+		updatesMenu.Label = fmt.Sprintf("%+v Updates Available", count)
 	}
 
-	tray.UpdateDependenciesMenu(updatesMenu, updates, count)
+	if count == 0 {
+		//systray.SetTemplateIcon(icon2x, icon)
+		//updateAllMenu.Disable()
+		updatesMenu.Disabled = true
+	} else {
+		//systray.SetIcon(icon2x)
+		//updateAllMenu.Enable()
+		updatesMenu.Disabled = false
+	}
+
+	// for _, update := range updates {
+	// 	label := fmt.Sprintf("%v (%v) -> %v", update.Name, strings.Join(update.Installed_Versions, ","), update.Current_Version)
+	// 	menu.AddSubMenuItem(label, "")
+	// }
+
+	menu.Refresh()
 }
--- internal/tray/CreateTrayMenu.go
+++ internal/tray/CreateTrayMenu.go
@@ -1,52 +1,80 @@
 package tray
 
 import (
-	"github.com/bealearts/kegger/internal/brew"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
 	. "github.com/bealearts/kegger/internal/logger"
-	"github.com/getlantern/systray"
-	"github.com/sqweek/dialog"
 )
 
-func CreateTrayMenu() (systray.MenuItem, systray.MenuItem) {
+var menu *fyne.Menu
 
-	mUpdates := systray.AddMenuItem("0 Updates Available", "")
-	mUpdates.Disable()
+func CreateTrayMenu() {
+	//updatesMenu := fyne.NewMenuItem("0 Updates", func() {})
 
-	mUpdate := systray.AddMenuItem("Update All", "")
-	mUpdates.Disable()
-	go func() {
-		<-mUpdate.ClickedCh
-		Logger.Info("Update All")
-		//brew.ExecCleanup()
-	}()
+	// menuItems := []*fyne.MenuItem{
+	// 	updatesMenu,
 
-	mClean := systray.AddMenuItem("Clean up Celler", "")
-	go func() {
-		<-mClean.ClickedCh
-		Logger.Info("Clean")
-		brew.ExecCleanup()
-	}()
+	// 	fyne.NewMenuItem("Clean up Celler", func() {
+	// 		updatesMenu.ChildMenu.Items = []*fyne.MenuItem{fyne.NewMenuItem("Changed", func() {
 
-	systray.AddSeparator()
+	// 		})}
+	// 		//menu.Refresh()
+	// 	}),
+	// 	fyne.NewMenuItemSeparator(),
+	// 	fyne.NewMenuItem("About", func() {
+	// 		Logger.Info("Hi")
+	// 		//dialog.Message("%s", "Do you want to continue?").Title("Are you sure?").Info()
+	// 	}),
+	// }
+	ap := app.New()
+	// menu = fyne.NewMenu("Kegger", menuItems...)
+	if desk, ok := ap.(desktop.App); ok {
+		menu = fyne.NewMenu("Kegger")
+		desk.SetSystemTrayMenu((menu))
+	}
+	Logger.Info("Starting")
+	ap.Run()
+	//return menu, updatesMenu
 
-	// mPref
+	// mUpdates := systray.AddMenuItem("0 Updates Available", "")
+	// mUpdates.Disable()
 
-	mAbout := systray.AddMenuItem("About", "")
-	go func() {
-		<-mAbout.ClickedCh
-		Logger.Info("About")
-		dialog.Message("%s", "Kegger - Join the party").Title("About").Info()
+	// mUpdate := systray.AddMenuItem("Update All", "")
+	// mUpdates.Disable()
+	// go func() {
+	// 	<-mUpdate.ClickedCh
+	// 	Logger.Info("Update All")
+	// 	//brew.ExecCleanup()
+	// }()
 
-	}()
+	// mClean := systray.AddMenuItem("Clean up Celler", "")
+	// go func() {
+	// 	<-mClean.ClickedCh
+	// 	Logger.Info("Clean")
+	// 	brew.ExecCleanup()
+	// }()
 
-	systray.AddSeparator()
+	// systray.AddSeparator()
 
-	mQuit := systray.AddMenuItem("Quit", "")
-	go func() {
-		<-mQuit.ClickedCh
-		Logger.Info("Quit")
-		systray.Quit()
-	}()
+	// // mPref
 
-	return *mUpdates, *mUpdate
+	// mAbout := systray.AddMenuItem("About", "")
+	// go func() {
+	// 	<-mAbout.ClickedCh
+	// 	Logger.Info("About")
+	// 	dialog.Message("%s", "Kegger - Join the party").Title("About").Info()
+
+	// }()
+
+	// systray.AddSeparator()
+
+	// mQuit := systray.AddMenuItem("Quit", "")
+	// go func() {
+	// 	<-mQuit.ClickedCh
+	// 	Logger.Info("Quit")
+	// 	systray.Quit()
+	// }()
+
+	// return *mUpdates, *mUpdate
 }
